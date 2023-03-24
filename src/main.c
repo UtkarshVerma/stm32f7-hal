433baa4f8feb89eefe049671672cee5e5073db09
Utkarsh Verma <utkarshverma@protonmail.com>
2023-03-24 11:25:31 +0530

    Clean up

--- src/main.c
+++ src/main.c
@@ -10,10 +10,9 @@ void init() {
 int main() {
 	init();
 
-	while (1) {
-		PWM_TIMER_CCR = (PWM_TIMER_CCR + 1) % pwmTimerHandle.Init.Period;
-		HAL_Delay(1);
-	}
+	PWM_TIMER_CCR = 0.15 * (pwmTimerHandle.Init.Period + 1);
 
+	while (1)
+		;
 	return 0;
 }
--- src/timers.c
+++ src/timers.c
@@ -57,22 +57,3 @@ void initTimers() {
 		errorHandler(__FILE__, __LINE__);
 	}
 }
-
-// //  Initializes the Global MSP.
-// void HAL_MspInit() {
-// 	__HAL_RCC_PWR_CLK_ENABLE();
-// 	__HAL_RCC_SYSCFG_CLK_ENABLE();
-
-// 	/* System interrupt init*/
-// 	/* PendSV_IRQn interrupt configuration */
-// 	HAL_NVIC_SetPriority(PendSV_IRQn, 15, 0);
-// }
-
-// void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim) {
-// 	/**TIM3 GPIO Configuration
-// 	PA6     ------> TIM3_CH1
-// 	PB1     ------> TIM3_CH4
-// 	PC7     ------> TIM3_CH2
-// 	PC8     ------> TIM3_CH3
-// 	*/
-// }
